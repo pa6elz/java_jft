64fab0de083ccea9f94193196b0cc1b0f74e4694
palthe <pa6elz@gmail.com>
2019-10-31 15:32:23 +0700

    Created packet for my first programm

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyClass1.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyClass1.java
@@ -1,24 +1,26 @@
-public class MyClass
+package ru.stqa.pft.sandbox;
+
+public class MyClass1
 {
   private int a;
   public double b;
   
-  public MyClass(int first, double second)
+  public MyClass1(int first, double second)
   {
     this.a = first;
     this.b = second;
   }
 
   // new method
-  public static void incrementBoth(MyClass c1) {
+  public static void incrementBoth(MyClass1 c1) {
     c1.a = c1.a + 1;
     c1.b = c1.b + 1.0;
   }
 
   public static void main(String[] args)
   {
-    MyClass c1 = new MyClass(10, 20.5);
-    MyClass c2 = new MyClass(10, 31.5);
+    MyClass1 c1 = new MyClass1(10, 20.5);
+    MyClass1 c2 = new MyClass1(10, 31.5);
     // different code below
     incrementBoth(c2);
     System.out.println(c1.a + ", "+ c2.a);
